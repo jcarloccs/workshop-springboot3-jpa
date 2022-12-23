73830082b4df9bf790f089afe97111d4d7205ff7
jcarloccs <jcarlo@outlook.com>
2022-12-23 10:32:34 -0300

    Service layer, component registration

---
+++ src/main/java/com/j/carlo/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.j.carlo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.j.carlo.course.entities.User;
+import com.j.carlo.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
