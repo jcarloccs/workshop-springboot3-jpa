4f9d5bf262f6ddfc86cf28e00120052a73669dab
jcarloccs <jcarlo@outlook.com>
2023-01-06 11:01:06 -0300

    Exception handling - update

--- src/main/java/com/j/carlo/course/services/UserService.java
+++ src/main/java/com/j/carlo/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.j.carlo.course.repositories.UserRepository;
 import com.j.carlo.course.services.exceptions.DatabaseException;
 import com.j.carlo.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
